702a7ceecad2e0c5f80c065c0b9c5a8a9bdf75b1
BONINI FRANCESCO <francesco.bonini97@yahoo.it>
2024-01-24 23:04:30 +0100

    Adding Pickable lantern

--- Source/ChickenRun/Private/Items/Item.cpp
+++ Source/ChickenRun/Private/Items/Item.cpp
@@ -9,6 +9,9 @@ AItem::AItem()
 {
 	PrimaryActorTick.bCanEverTick = true;
 
+	RootComponent = CreateDefaultSubobject<USceneComponent>(TEXT("Root"));
+	SetRootComponent(RootComponent);
+	
 	ItemMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("ItemMeshComponent"));
 	ItemMesh->SetGenerateOverlapEvents(false);
 	ItemMesh->SetCollisionEnabled(ECollisionEnabled::PhysicsOnly);
@@ -17,11 +20,11 @@ AItem::AItem()
 	ItemMesh->SetCollisionResponseToChannel(ECC_WorldStatic, ECR_Block);
 	ItemMesh->SetCollisionResponseToChannel(ECC_WorldDynamic, ECR_Block);
 	ItemMesh->SetCollisionResponseToChannel(ECC_Pawn, ECR_Block);
-	ItemMesh->SetCollisionResponseToChannel(ECC_PhysicsBody, ECR_Block);
-	RootComponent = ItemMesh;
+	ItemMesh->SetCollisionResponseToChannel(ECC_PhysicsBody, ECR_Block);	
+	ItemMesh->SetupAttachment(RootComponent);
 
 	SphereTrigger = CreateDefaultSubobject<USphereComponent>(TEXT("SphereTrigger"));
-	SphereTrigger->SetupAttachment(GetRootComponent());
+	SphereTrigger->SetupAttachment(RootComponent);
 	SphereTrigger->SetGenerateOverlapEvents(false);
 	SphereTrigger->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
 	SphereTrigger->SetCollisionObjectType(ECC_WorldDynamic);
--- Source/ChickenRun/Public/Items/Item.h
+++ Source/ChickenRun/Public/Items/Item.h
@@ -24,7 +24,7 @@ protected:
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Components")
 	TObjectPtr<UStaticMeshComponent> ItemMesh;
 
-	UPROPERTY(VisibleAnywhere, Category="Components")
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Components")
 	TObjectPtr<USphereComponent> SphereTrigger;
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite)
